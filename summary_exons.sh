f59a274ab120f0df8b0927c1b8a3571b77526826
rhamb001 <56095839+rhamb001@users.noreply.github.com>
2019-10-16 09:56:31 -0700

    Update summary_exons.sh

--- summary_exons.sh
+++ summary_exons.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/bash
 # your code goes next
+countexon=0 # set starting variable to 0
+curl -o rice.bed https://raw.githubusercontent.com/biodataprog/GEN220/master/data/rice_random_exons.bed #save file as rice.bed
+exondiff=$(awk '{print $3-$2}' rice.bed) #subtract column 2 from 3 to get all exon lengths and save this data as exondiff
+for num in ${exondiff[@]} #for loop read all lines and add into one variable
+do
+countexon=$(( $num +  $countexon ))
+done
+echo The total length of the exons is: $countexon
