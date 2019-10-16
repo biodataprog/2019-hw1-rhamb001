c43eab4ccb506cbb74b74e5e220dbccf16fa99d0
rhamb001 <56095839+rhamb001@users.noreply.github.com>
2019-10-16 09:48:23 -0700

    Update download_count.sh

--- download_count.sh
+++ download_count.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash
 # your script goes below here
+curl -o blast.gz ftp://ftp.ncbi.nih.gov//blast/db/FASTA/vector.gz #open the file and save under name blast.gz
+gunzip blast.gz #unzip the file, it is now just named blast
+echo The number of FASTA format sequences in this file is: $(grep -c ">" blast) #count the number of times ">" symbol is in file
